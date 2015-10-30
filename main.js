9edd441937f654d6406a3d0ab6d2a2bdc34304f9
benhassara <bhassara@gmail.com>
2015-10-30 10:37:18 -0600

    chained named functions

--- main.js
+++ main.js
@@ -3,10 +3,34 @@
 var ourResult = true;
 
 var promise = new Promise(function(resolve, reject) {
-    if (ourResult) {resolve('It worked!');}
-    else {reject(':()');}
+    if (ourResult) { resolve('It worked'); }
+    else { reject(':('); }
 });
 
-promise.then(function(result) {
-    console.log('Result: ', result);
-});
+// promise.then(function(result) {
+//     console.log('Result: ', result);
+// }).catch(function(error) {
+//     console.log('Error: ', error);
+// });
+
+// catch is more explicit, but the below means the same thing
+// catch is actually an alias for this, but the syntactic sugar
+// makes it more apparent what's going on semantically
+//
+// promise.then(function(result) {
+//     console.log('Result: ', result);
+// }, function(error) {
+//     console.log('Error: ', error);
+// });
+
+
+// there's also another way to write it, with named functions
+
+function successFn(message) {
+    console.log(message);
+    return message + '!!!!!!!!!!!!!';
+}
+function errorFn(message) { console.log('Error: ', message); }
+
+promise.then(successFn, errorFn)
+       .then(successFn, errorFn);
